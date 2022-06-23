57c12a02e7821a60cd1ca759db2b3b2fee86a301
Shanmugaapriyan <shanmugaapriyan04@gmail.com>
2022-06-23 12:52:22 +0530

    All files

---
+++ 3_Implementation/src/exit.c
@@ -0,0 +1,25 @@
+/**
+ * @file exit.c
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+int menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
+    return 5;
+
+}//exit menu
+
+ 
+
+   
